74689d45bbc92dc0c0e33a6bef44158a81c45a9b
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2025-09-07 23:05:00 +0100

    + Fixed review date on page

--- OnlineShop/ViewModels/ReviewViewModel.cs
+++ OnlineShop/ViewModels/ReviewViewModel.cs
@@ -8,6 +8,6 @@
         public string Comment { get; set; } = string.Empty;
         public string CustomerName => $"{Customer.FirstName} {Customer.LastName}";
         public CustomerViewModel Customer { get; set; }
-        public DateTime DateCreated { get; internal set; }
+        public DateTime CreatedAt { get; internal set; }
     }
 }
