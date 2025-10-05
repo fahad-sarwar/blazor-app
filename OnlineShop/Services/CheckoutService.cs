ae645d5e6341a944cc8ba3b7ce9d1a04911b8dca
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2025-10-05 14:37:01 +0100

    + removed reference to bogus

--- OnlineShop/Services/CheckoutService.cs
+++ OnlineShop/Services/CheckoutService.cs
@@ -12,28 +12,6 @@ namespace OnlineShopUI.Services
             var lastName = user.FindFirst(ClaimTypes.Surname)?.Value ?? string.Empty;
             var email = user.FindFirst(ClaimTypes.Email)?.Value ?? string.Empty;
 
-            // TODO: remove this code and the nuget package
-            //var testUser = new Faker<CheckoutViewModel>("en_GB")
-            //    .RuleFor(c => c.CustomerId, f => customerId)
-            //    .RuleFor(c => c.FirstName, f => firstName)
-            //    .RuleFor(c => c.LastName, f => lastName)
-            //    .RuleFor(c => c.Email, f => email)
-            //    .RuleFor(c => c.PhoneNumber, f => f.Phone.PhoneNumber())
-            //    .RuleFor(c => c.BillingAddressLineOne, f => f.Address.StreetAddress())
-            //    .RuleFor(c => c.BillingTown, f => f.Address.City())
-            //    .RuleFor(c => c.BillingCounty, f => f.Address.County())
-            //    .RuleFor(c => c.BillingPostCode, f => f.Address.ZipCode("??# #??"))
-            //    .RuleFor(c => c.ShippingAddressLineOne, f => f.Address.StreetAddress())
-            //    .RuleFor(c => c.ShippingTown, f => f.Address.City())
-            //    .RuleFor(c => c.ShippingCounty, f => f.Address.County())
-            //    .RuleFor(c => c.ShippingPostCode, f => f.Address.ZipCode("??# #??"))
-            //    .RuleFor(c => c.CardName, (f, vm) => $"{vm.FirstName} {vm.LastName}")
-            //    .RuleFor(c => c.CardNumber, "4111111111111111")
-            //    .RuleFor(c => c.Expiry, "12/28")
-            //    .RuleFor(c => c.CVV, "123");
-
-            //var checkoutViewModel = testUser.Generate();
-
             var checkoutViewModel = new CheckoutViewModel
             {
                 CustomerId = customerId,
