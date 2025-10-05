0c56f5f98ba327c7d7bbd8bb7085c31a32429829
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2025-10-05 12:40:51 +0100

    + refactored FAQs to be more generic

---
+++ OnlineShop/ViewModels/FaqQuestionViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShopUI.ViewModels
+{
+    public class FaqQuestionViewModel
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ OnlineShop/ViewModels/FaqSectionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopUI.ViewModels
+{
+    public class FaqSectionViewModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public FaqQuestionViewModel Question { get; set; }
+    }
+}
